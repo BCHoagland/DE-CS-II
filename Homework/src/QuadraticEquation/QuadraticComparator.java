da204d33cb40020254186e7a03d57db4a93afa6d
HoaglandB1 <HoaglandB1@104-09-2017.bi.local>
2017-09-12 14:07:53 -0400

    started QuadraticEquation changes

---
+++ Homework/src/QuadraticEquation/QuadraticComparator.java
@@ -0,0 +1,12 @@
+package QuadraticEquation;
+
+import java.util.Comparator;
+
+public class QuadraticComparator implements Comparator<QuadraticEquation> {
+
+	public int compare(QuadraticEquation eq1, QuadraticEquation eq2) {
+		return 0;
+		
+	}
+
+}

2ea42679c7144378b6b913a5920409c3cc0c309e
HoaglandB1 <HoaglandB1@104-09-2017.bi.local>
2017-09-19 11:15:21 -0400

    added all choosingWords filse

--- Labs/src/choosingWords/ChoosingWords.java
+++ Labs/src/choosingWords/ChoosingWords.java
@@ -2,6 +2,7 @@ package choosingWords;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class ChoosingWords {
@@ -20,15 +21,33 @@ public class ChoosingWords {
 		return input;
 	}
 	
+	public static PrintWriter openDictionary(String fileName) {
+		File file = new File(fileName);
+		PrintWriter output = null;
+		
+		try {
+			output = new PrintWriter(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Can't open file: " + fileName);
+			return null;
+		}
+		
+		return output;
+	}
+	
 	public static void main(String[] args) {
-		if (args.length < 1)  {
+		if (args.length < 2)  {
 			System.out.println("You did not supply a file to read");
 			System.exit(1);
 		}
 		
 		Scanner in = openWords(args[0]);
 		if (in == null) System.exit(1);
-		System.out.println(in.nextLine());
+		
+		PrintWriter out = openDictionary(args[1]);
+		out.println(in.nextLine());
+		
 		in.close();
+		out.close();
 	}
 }

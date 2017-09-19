c11f68857ea60dd2d31f6450ed24f579ed123854
HoaglandB1 <HoaglandB1@104-09-2017.bi.local>
2017-09-19 11:09:37 -0400

    updated ChoosingWords files

--- Labs/src/choosingWords/ChoosingWords.java
+++ Labs/src/choosingWords/ChoosingWords.java
@@ -1,8 +1,34 @@
 package choosingWords;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ChoosingWords {
 	
+	public static Scanner openWords(String fileName) {
+		File file = new File(fileName);
+		Scanner input = null;
+		
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Can't open file: " + fileName);
+			return null;
+		}
+		
+		return input;
+	}
 	
+	public static void main(String[] args) {
+		if (args.length < 1)  {
+			System.out.println("You did not supply a file to read");
+			System.exit(1);
+		}
+		
+		Scanner in = openWords(args[0]);
+		if (in == null) System.exit(1);
+		System.out.println(in.nextLine());
+		in.close();
+	}
 }

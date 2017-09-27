1f6d001bf06dbc2ceac0f59bc72833c2580b09fc
HoaglandB1 <HoaglandB1@104-09-2017.bi.local>
2017-09-27 10:41:37 -0400

    updated Deck default constructor

--- Homework/src/selectionAndMergeSorts/Deck.java
+++ Homework/src/selectionAndMergeSorts/Deck.java
@@ -3,4 +3,16 @@ package selectionAndMergeSorts;
 public class Deck {
 	private Card[] cards;
 	private int topIndex;
+	
+	public Deck() {
+		this.cards = new Card[52];
+		int index = 0;
+		for (int suit = 0; suit < 4; suit++) {
+			for (int rank = 1; rank < 14; rank++) {
+				Card card = new Card(suit, rank);
+				cards[index] = card;
+				index++;
+			}
+		}
+	}
 }

9ad09c0d6fb092bb65c8af337a9e7f0d2e924da3
BCHoagland <bhoggland@gmail.com>
2017-10-05 09:30:12 -0400

    Update TestDeck.java

--- Homework/src/selectionAndMergeSorts/TestDeck.java
+++ Homework/src/selectionAndMergeSorts/TestDeck.java
@@ -1,8 +1,5 @@
 package selectionAndMergeSorts;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 public class TestDeck {
 
 	public static void main(String[] args) {
@@ -17,23 +14,6 @@ public class TestDeck {
 		testDeck.selectionSort();
 		for (Card card : testDeck.getCards()) {
 			System.out.println(card);
-		}
-
-
-
-
-
-		PrintWriter outputFile;
-
-		try {
-			outputFile = new PrintWriter("output.txt");
-		} catch (FileNotFoundException ex) {
-			System.out.println("Unable to open output file. Is it in the project folder?");
-			outputFile = null;
-		}
-
-		if (outputFile != null) {
-			outputFile.println();
 		}
 	}
 }

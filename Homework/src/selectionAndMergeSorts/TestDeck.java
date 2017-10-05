e05aa752259e395ad4504dd63639fc8dce355a44
HoaglandB1 <HoaglandB1@104-03-2017.bi.local>
2017-10-05 09:28:16 -0400

    added new branch

--- Homework/src/selectionAndMergeSorts/TestDeck.java
+++ Homework/src/selectionAndMergeSorts/TestDeck.java
@@ -1,5 +1,8 @@
 package selectionAndMergeSorts;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class TestDeck {
 
 	public static void main(String[] args) {
@@ -9,11 +12,28 @@ public class TestDeck {
 			System.out.println(card);
 		}
 		System.out.println();
-		
+
 		testDeck = new Deck(false);
 		testDeck.selectionSort();
 		for (Card card : testDeck.getCards()) {
 			System.out.println(card);
+		}
+
+
+
+
+
+		PrintWriter outputFile;
+
+		try {
+			outputFile = new PrintWriter("output.txt");
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open output file. Is it in the project folder?");
+			outputFile = null;
+		}
+
+		if (outputFile != null) {
+			outputFile.println();
 		}
 	}
 }

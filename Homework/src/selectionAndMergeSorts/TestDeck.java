409a73d6ed666612f8e061f6148f1ccf145b4a36
BCHoagland <bhoggland@gmail.com>
2017-10-05 10:19:55 -0400

    finished output file for real this time. but who knows I may change it again

--- Homework/src/selectionAndMergeSorts/TestDeck.java
+++ Homework/src/selectionAndMergeSorts/TestDeck.java
@@ -6,29 +6,27 @@ import java.io.PrintWriter;
 public class TestDeck {
 
 	public static void main(String[] args) {
-
-
-
-
-
+		
+		//create PrintWriter to write to output file
 		PrintWriter outputFile;
-
 		try {
 			outputFile = new PrintWriter("output.txt");
 		} catch (FileNotFoundException ex) {
 			System.out.println("Unable to open output file. Is it in the project folder?");
 			outputFile = null;
 		}
-
+		
 		if (outputFile != null) {
-			Deck testDeck = new Deck(false);
 			
+			//unsorted deck
 			outputFile.println("-------------\nUNSORTED DECK\n-------------");
+			Deck testDeck = new Deck(false);
 			for (Card card : testDeck.getCards()) {
 				outputFile.println(card);
 			}
 			outputFile.println();
 			
+			//test merge sort
 			outputFile.println("---------------\nMERGE SORT DECK\n---------------");
 			testDeck.mergeSort();
 			for (Card card : testDeck.getCards()) {
@@ -36,6 +34,7 @@ public class TestDeck {
 			}
 			outputFile.println();
 			
+			//test selection sort
 			outputFile.println("-------------------\nSELECTION SORT DECK\n-------------------");
 			testDeck = new Deck(false);
 			testDeck.selectionSort();
@@ -44,21 +43,29 @@ public class TestDeck {
 			}
 			outputFile.println();
 			
+			//test Deck toString method
 			outputFile.println("-------------\nDECK TOSTRING\n-------------");
 			outputFile.println(testDeck);
 			outputFile.println();
 			
+			//test Deck deal method
 			outputFile.println("----\nDEAL\n----");
 			testDeck.shuffle();
 			Deck[] decks = testDeck.deal(3, 6);
 			for (int i = 0; i < decks.length; i++) {
 				outputFile.println("HAND " + (i + 1) + "\n" + decks[i] + "\n\n");
 			}
-			
-			outputFile.println("----\nPICK\n----");
-			outputFile.println(testDeck.pick());
+			outputFile.println("REMAINING CARDS IN DECK:\n" + testDeck);
 			outputFile.println();
 			
+			//test Deck pick method
+			outputFile.println("----\nPICK\n----");
+			testDeck = new Deck(false);
+			outputFile.println("cards should be randomly selected: " + testDeck.pick() + ", " + testDeck.pick() + "\n");
+			outputFile.println("REMAINING CARDS IN DECK:\n" + testDeck);
+			outputFile.println();
+			
+			//test Deck equals method
 			outputFile.println("------\nEQUALS\n------");
 			testDeck = new Deck(true);
 			Deck testDeck2 = new Deck(true);

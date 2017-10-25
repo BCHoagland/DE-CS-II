8fa1744b0c3825e488a8bdc0d78a099a93057dd0
HoaglandB1 <HoaglandB1@104-02-2017.bi.local>
2017-10-25 14:07:25 -0400

    stock exchange stuff

---
+++ Labs/src/stockExchange/SafeTradeTest.java
@@ -0,0 +1,10 @@
+package stockExchange;
+
+public class SafeTradeTest {
+
+	public static void main(String[] args) {
+		Stock s = new Stock("MSFT", "Microsoft", 4000.00);
+		
+		System.out.println(s);
+	}
+}
--- Labs/src/stockExchange/Stock.java
+++ Labs/src/stockExchange/Stock.java
@@ -1,5 +1,58 @@
 package stockExchange;
 
-public class Stock {
+import java.text.DecimalFormat;
+import java.util.PriorityQueue;
 
+public class Stock implements Comparable<Stock> {
+	
+	private String symbol;
+	private String name;
+	private double lowPrice, lastPrice, highPrice;
+	
+	private PriorityQueue<TradeOrder> buyOrders;
+	private PriorityQueue<TradeOrder> sellOrders;
+	
+	private static DecimalFormat money = new DecimalFormat("$#,##0.00");
+	
+	Stock(String s, String n, double p) {
+		this.symbol = s;
+		this.name = n;
+		this.lastPrice = p;
+		this.lowPrice = p;
+		this.highPrice = p;
+	}
+	
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getLowPrice() {
+		return lowPrice;
+	}
+
+	public double getLastPrice() {
+		return lastPrice;
+	}
+
+	public double getHighPrice() {
+		return highPrice;
+	}
+
+	public String toString() {
+		return this.getSymbol() + ", " + this.getName() + ", " + money.format(this.getLastPrice());
+	}
+	
+	public void placeOrder(TradeOrder order) {
+		
+	}
+
+	@Override
+	public int compareTo(Stock s) {
+		//TODO finish this
+		return (int)(this.getLastPrice() - s.getLastPrice());
+	}
 }

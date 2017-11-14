e04ccfcca33e10dca835c8bdc45058f18031dcda
HoaglandB1 <HoaglandB1@104-02-2017.bi.local>
2017-11-14 10:57:46 -0500

    fun tests

--- Homework/src/indexMaker/IndexMakerTest.java
+++ Homework/src/indexMaker/IndexMakerTest.java
@@ -3,12 +3,31 @@ package indexMaker;
 import java.io.IOException;
 
 public class IndexMakerTest {
-	
+
 	public static void main(String[] args) throws IOException {
+		normalTests();
+//		ohYeah(50);
+	}
+
+	public static void normalTests() throws IOException {
 		String[][] fileNames = {{"fish.txt", "output.txt"},
-								{"lmao", "output2.txt"}};
-		
+				{"lmao", "output2.txt"}};
+
 		for (String[] files : fileNames) {
+			try {
+				IndexMaker.main(files);
+			} finally {}
+		}
+	}
+
+	public static void ohYeah(int numOutputFiles) throws IOException {
+		for (int i = 0; i < numOutputFiles; i++) {
+			String inputFile = "output" + i + ".txt";
+			if (i == 0) {
+				inputFile = "fish.txt";
+			}
+			String outputFile = "output" + (i + 1) + ".txt";
+			String[] files = {inputFile, outputFile};
 			try {
 				IndexMaker.main(files);
 			} finally {}

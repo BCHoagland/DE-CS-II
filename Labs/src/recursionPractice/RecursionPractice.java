bcf7933fda04be692bc49b91e691dffec9be7cf1
HoaglandB1 <HoaglandB1@104-02-2017.bi.local>
2018-01-09 14:57:52 -0500

    stuff

--- Labs/src/recursionPractice/RecursionPractice.java
+++ Labs/src/recursionPractice/RecursionPractice.java
@@ -14,7 +14,7 @@ public class RecursionPractice {
 	
 	public static int factorial(int n) {
 		if (n <= 1) {
-			return n;
+			return 1;
 		}
 		return n * factorial(n - 1);
 	}
@@ -73,16 +73,85 @@ public class RecursionPractice {
 		}
 	}
 	
+	public static void printTitle(String str) {
+		String title = "TESTING " + str.toUpperCase();
+		String border = "\n";
+		for (int i = 0; i < title.length(); i++) {
+			border += "=";
+		}
+		System.out.println(border + "\n" + title + border);
+	}
+	
+	public static void testFibonacci() {
+		printTitle("fibonacci");
+		for (int i = 0; i < 10; i++) {
+			int num = (int)(Math.random() * 10);
+			String ending = "th";
+			if (num == 1) ending = "st";
+			else if (num == 2) ending = "nd";
+			else if (num == 3) ending = "rd";
+			System.out.println("the " + num + ending + " fibonacci number is " + fibonacci(num));
+		}
+	}
+	
+	public static void testFactorial() {
+		printTitle("factorial");
+		for (int i = 0; i < 10; i++) {
+			int num = (int)(Math.random() * 10);
+			System.out.println(num + "! = " + factorial(num));
+		}
+	}
+	
+	public static void testSumDigits() {
+		printTitle("sum digits");
+		for (int i = 0; i < 10; i++) {
+			int num = (int)(Math.random() * 100000);
+			System.out.println("the sum of the digits in " + num + " is " + sumDigits(num));
+		}
+	}
+	
+	public static void testCount5() {
+		printTitle("count 5");
+		for (int i = 0; i < 10; i++) {
+			int num = (int)(Math.random() * 100000);
+			System.out.println("the number of 5s in " + num + " is " + count5(num));
+		}
+	}
+	
+	public static void testUpAndDown() {
+		printTitle("up and down");
+		for (int i = 0; i < 10; i++) {
+			int num = (int)(Math.random() * 10);
+			System.out.print("up and down " + num + ": ");
+			upAndDown(num);
+			System.out.println();
+		}
+	}
+	
+	public static void testMax() {
+		printTitle("max");
+		for (int i = 0; i < 10; i++) {
+			double[] nums = new double[10];
+			for (int j = 0; j < nums.length; j++) {
+				nums[j] = Math.random() * 10;
+			}
+			int num = (int)(Math.random() * (nums.length - 1)) + 1;
+			String numsString = "{";
+			for (double n : nums) {
+				numsString += n + ", ";
+			}
+			numsString += "}";
+			System.out.println("the max number of the first " + num + " numbers in " + numsString + " is " + max(nums, num));
+		}
+	}
+	
 	public static void main(String[] args) {
-//		System.out.println(fibonacci(6));
-//		System.out.println(factorial(4));
-//		System.out.println(sumDigits(123));
-//		System.out.println(count5(45456789));
-//		upAndDown(5);
-		
-		double[] nums = {4, 5, 6, 7, 3, 6};
-		System.out.println(max(nums, 5));
-		
-		System.out.println(isDivisibleBy9(90));
+		testFibonacci();
+		testFactorial();
+		testSumDigits();
+		testCount5();
+		testUpAndDown();
+		testMax();
+		System.out.println("\nI didn't make a test method for isDivisibleBy9() oops\n" + isDivisibleBy9(90));
 	}
 }

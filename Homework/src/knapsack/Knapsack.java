1efb2cc6da16528d26a48de86e53269fffa8b5a8
HoaglandB1 <HoaglandB1@104-08-2017.bi.local>
2018-02-22 10:34:46 -0500

    stuff from class

--- Homework/src/knapsack/Knapsack.java
+++ Homework/src/knapsack/Knapsack.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Knapsack {
-	
+
 	private static int[] testCapacities = {165, 26, 190, 750};
 	private static int[][] testWeights = {
 			{23, 31, 29, 44, 53, 38, 63, 85, 89, 82},
@@ -22,47 +22,51 @@ public class Knapsack {
 			{0, 1, 1, 1, 0},
 			{1, 1, 0, 0, 1, 0},
 			{1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1}};
-	
+
 	private static ArrayList<Integer> sets = new ArrayList<Integer>();
+	
+	private static int knapsack(int capacity, int n, int[] weights, int[] values) {
+		int maxValue = knapsack(capacity, n, weights, values, sets);
+		Collections.reverse(sets);
+		return maxValue;
+	}
 	
 	private static int knapsack(int capacity, int n, int[] weights, int[] values, ArrayList<Integer> list) {
 		ArrayList<Integer> list1 = new ArrayList<Integer>();
 		ArrayList<Integer> list2 = new ArrayList<Integer>();
+//		list1.add(0);
+//		list2.add(1);
 		
-		if (n < 0 || capacity <= 0) {
+		if (n < 0) {
 			return 0;
 		}
 		
-		//REVERSE SETS VARIABLE AFTER YOU'RE DONE WITH IT
-		
-		
-		list1.add(0);
-		list2.add(1);
-//		list2.add(weights[n]);
-		
+		list2.add(values[n]);
+
+		if (capacity <= 0) {
+			list.addAll(list1);
+			return 0;
+		}
+
 		if (weights[n] > capacity) {
 			list.addAll(list1);
 			return knapsack(capacity, n - 1, weights, values, list1);
 		}
-		else {
-			int incCurrent = values[n] + knapsack(capacity - weights[n], n - 1, weights, values, list2);
-			int excCurrent = knapsack(capacity, n - 1, weights, values, list1);
-			if (incCurrent > excCurrent) {
-				list.addAll(list2);
-				return incCurrent;
-			}
-			list.addAll(list1);
-			return excCurrent;
+		
+		int incCurrent = values[n] + knapsack(capacity - weights[n], n - 1, weights, values, list2);
+		int excCurrent = knapsack(capacity, n - 1, weights, values, list1);
+		if (incCurrent > excCurrent) {
+			list.addAll(list2);
+			return incCurrent;
 		}
+		list.addAll(list1);
+		return excCurrent;
 	}
-	
+
 	public static void main(String[] args) {
 		for (int i = 0; i < testCapacities.length; i++) {
 			sets.clear();
-//			for (int j = 0; j < testWeights[i].length; j++) {
-//				sets.add(0);
-//			}
-			
+
 			System.out.print("capacity: " + testCapacities[i] + "\nweights: ");
 			for (int num : testWeights[i]) {
 				System.out.print(num + " ");
@@ -71,7 +75,10 @@ public class Knapsack {
 			for (int num : testValues[i]) {
 				System.out.print(num + " ");
 			}
-			System.out.print("\nmax value: " + knapsack(testCapacities[i], testWeights[i].length - 1, testWeights[i], testValues[i], sets) + " -> correct max value: " + testAnswerValues[i]);
+
+			int numTypes = testWeights[i].length;
+			System.out.print("\nmax value: " + knapsack(testCapacities[i], numTypes - 1, testWeights[i], testValues[i]) + " -> correct max value: " + testAnswerValues[i]);
+
 			System.out.print("\nobjects chosen: " + sets + " -> " + "correct objects: [");
 			for (int num : testAnswerSets[i]) {
 				System.out.print(num + ", ");

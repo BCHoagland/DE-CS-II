3a95b2c54d9c867673b252099ce20a9ad05e4d7d
BCHoagland <bhoggland@gmail.com>
2018-02-24 19:51:04 -0500

    tons of progress

--- Homework/src/knapsack/Knapsack.java
+++ Homework/src/knapsack/Knapsack.java
@@ -3,31 +3,27 @@ package knapsack;
 import java.awt.List;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Knapsack {
 	
-	private static ArrayList<Integer> set = new ArrayList<Integer>();
+	private static final String OUTPUT_FILE_NAME = "knapsack.txt";
 	
-	private static ArrayList<String> getTestFileNames() {
-		//TODO: ASK FOR USER INPUT TO DETERMINE NAME OF FILE WITH TEST FILES
+	private static ArrayList<Integer> set = new ArrayList<Integer>();
+	private static String outputStr = "";
+	
+	private static ArrayList<String> getTestFileNames(Scanner testFiles) {
 		ArrayList<String> testFileNames = new ArrayList<String>();
-		
-		Scanner testFiles = null;
-		try {
-			testFiles = new Scanner(new File("tests.txt"));
-		} catch (FileNotFoundException ex) {
-			System.out.println(ex);
-			System.exit(1);
-		}
 
 		if (testFiles != null) {
 			while (testFiles.hasNextLine()) {
 				testFileNames.add(testFiles.nextLine().trim());
 			}
-			System.out.println(testFileNames);
 			testFiles.close();
 		}
 		return testFileNames;
@@ -42,8 +38,7 @@ public class Knapsack {
 		try {
 			lines = new Scanner(new File(fileName));
 		} catch (FileNotFoundException ex) {
-			System.out.println(ex);
-			System.exit(1);
+			System.out.println(fileName + " isn't an actual file so it's been skipped");
 		}
 		
 		if (lines != null) {
@@ -67,16 +62,62 @@ public class Knapsack {
 			r[i] = values.get(i);
 		}
 		
-		return knapsack(w, r, w.length - 1, limit, set);
+		int maxValue = knapsack(w, r, w.length - 1, limit, set);
+		
+		outputStr += getOutputStr(fileName, limit, weights, values);
+		
+		return maxValue;
 	}
 	
-	private static void runAllTests() {
-		ArrayList<String> testFiles = getTestFileNames();
+	private static String getOutputStr(String fileName, int limit, ArrayList<Integer> weights, ArrayList<Integer> values) {
+		String finalStr = "";
+		
+		if (limit == -1) {
+			return finalStr;
+		}
+
+		String weightsStr = weights.toString();
+		String valuesStr = values.toString();
+		finalStr += fileName + "\t" + limit + "\t" + weightsStr.substring(1, weightsStr.length() - 1) + "\t" + valuesStr.substring(1, valuesStr.length() - 1) + "\n";
+
+		ArrayList<Integer> weightsWithoutRepeats = new ArrayList<Integer>();
+		ArrayList<Integer> amounts = new ArrayList<Integer>();
+		for (int weight : weights) {
+			if (!weightsWithoutRepeats.contains(weight)) {
+				weightsWithoutRepeats.add(weight);
+				amounts.add(1);
+			} else {
+				int index = weightsWithoutRepeats.indexOf(weight);
+				amounts.set(index, amounts.get(index) + 1);
+			}
+		}
+		for (int i = 0; i < amounts.size(); i++) {
+			finalStr += "\n" + amounts.get(i) + " " + weightsWithoutRepeats.get(i) + " pound watermelons";
+		}
+
+		finalStr += "\n\n\n";
+		return finalStr;
+	}
+	
+	private static void runAllTests(Scanner testFilesScanner) {
+		ArrayList<String> testFiles = getTestFileNames(testFilesScanner);
 		
 		int maxValue;
 		for (String fileName : testFiles) {
 			maxValue = runKnapsackOnFile(fileName);
-			System.out.println(maxValue + ", " + set);
+		}
+		
+		PrintWriter output = null;
+		try {
+			output = new PrintWriter(new File(OUTPUT_FILE_NAME));
+		} catch (FileNotFoundException ex) {
+			System.out.println(ex);
+			System.exit(1);
+		}
+		
+		if (output != null) {
+			output.print(outputStr.trim());
+			output.close();
 		}
 	}
 	
@@ -134,7 +175,40 @@ public class Knapsack {
 	}
 
 	public static void main(String[] args) {
-		runAllTests();
-		System.out.println("Results should be: 309, 51, 150, 1458");
+		if (args.length > 0) {
+			String inputFileName = args[0];
+			
+			Scanner kb = new Scanner(System.in);
+			Scanner testFiles = null;
+			while (testFiles == null) {
+				try {
+					testFiles = new Scanner(new File(inputFileName));
+				} catch (FileNotFoundException ex) {
+					System.out.println("Please enter the name of an actual file:");
+					inputFileName = kb.next();
+				}
+			}
+			kb.close();
+			
+			runAllTests(testFiles);
+		} else {
+			Scanner kb = new Scanner(System.in);
+			System.out.println("Please enter your file name: ");
+			String inputFileName = kb.next();
+			
+			Scanner testFiles = null;
+			while (testFiles == null) {
+				try {
+					testFiles = new Scanner(new File(inputFileName));
+				} catch (FileNotFoundException ex) {
+					System.out.println("Please enter the name of an actual file:");
+					inputFileName = kb.next();
+				}
+			}
+			kb.close();
+			
+			runAllTests(testFiles);
+			System.out.println("All tests run");
+		}
 	}
 }

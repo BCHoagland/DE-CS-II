f4fcc4c9fc4e930e4d657df945308c2f5f13ca28
BCHoagland <bhoggland@gmail.com>
2018-02-28 20:08:42 -0500

    bhoag's bar and grill has a frame with no structural testing whatsoever

--- Homework/src/expressionTrees/ExpressionTree.java
+++ Homework/src/expressionTrees/ExpressionTree.java
@@ -11,18 +11,31 @@ public class ExpressionTree extends TreeNode implements Expressions {
 		return Arrays.asList(OPERATORS).contains(str);
 	}
 	
+	public int runOperation(String operand, int n1, int n2) {
+		switch (operand) {
+			case "*":
+				return n1 * n2;
+			case "+":
+				return n1 + n2;
+			default:
+				return -1;
+		}
+	}
+	
 	@Override
 	public TreeNode buildTree(String[] exp) {
 		Stack<TreeNode> stack = new Stack<TreeNode>();
 		for (String ch : exp) {
 			if (isOperator(ch)) {
-				//make operator a node that's a parent to the two top nodes on the stack
-				//push operator node to stack
+				TreeNode operatorNode = new TreeNode(ch);
+				operatorNode.setRight(stack.pop());
+				operatorNode.setLeft(stack.pop());
+				stack.push(operatorNode);
 			} else {
 				stack.push(new TreeNode(ch));
 			}
 		}
-		return null;
+		return stack.pop();
 	}
 
 	@Override
@@ -30,11 +43,33 @@ public class ExpressionTree extends TreeNode implements Expressions {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	public int evalTree(TreeNode node) {
+		if (node != null) {
+			String nodeValue = node.getValue().toString();
+			if (isOperator(nodeValue)) {
+				return runOperation(nodeValue, evalTree(node.getLeft()), evalTree(node.getRight()));
+			} else {
+				return Integer.parseInt(nodeValue);
+			}
+		} else {
+			return 0;
+		}
+	}
 
 	@Override
 	public String toPrefixNotation() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void toPrefixNotation(TreeNode node, String str) {
+		if (node != null) {
+			String nodeValue = node.getValue().toString();
+			str += nodeValue;
+			toPrefixNotation(node.getLeft(), str);
+			toPrefixNotation(node.getRight(), str);
+		}
 	}
 
 	@Override
@@ -42,17 +77,44 @@ public class ExpressionTree extends TreeNode implements Expressions {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void toInfixNotation(TreeNode node, String str) {
+		if (node != null) {
+			String nodeValue = node.getValue().toString();
+			toInfixNotation(node.getLeft(), str);
+			str += nodeValue;
+			toInfixNotation(node.getRight(), str);
+		}
+	}
 
 	@Override
 	public String toPostfixNotation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void toPostfixNotation(TreeNode node, String str) {
+		if (node != null) {
+			String nodeValue = node.getValue().toString();
+			toPostfixNotation(node.getLeft(), str);
+			str += nodeValue;
+			toPostfixNotation(node.getRight(), str);
+		}
+	}
 
 	@Override
 	public int postfixEval(String[] exp) {
-		// TODO Auto-generated method stub
-		return 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		for (String ch : exp) {
+			int value;
+			if (isOperator(ch)) {
+				value = runOperation(ch, stack.pop(), stack.pop());
+			} else {
+				value = Integer.parseInt(ch);
+			}
+			stack.push(value);
+		}
+		return stack.pop();
 	}
 	
 	

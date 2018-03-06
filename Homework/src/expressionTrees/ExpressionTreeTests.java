a3d5f344228c1fbacbccee8714f0bd9535ddd273
BCHoagland <bhoggland@gmail.com>
2018-03-05 19:56:50 -0500

    bhoag's bar and grill now has a parking lot

--- Homework/src/expressionTrees/ExpressionTreeTests.java
+++ Homework/src/expressionTrees/ExpressionTreeTests.java
@@ -2,6 +2,7 @@ package expressionTrees;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,6 +12,29 @@ import java.util.Scanner;
  * <h2>Class to test the ExpressionTree class</h2>
  */
 public class ExpressionTreeTests {
+	
+	public static final String outputFileName = "Hoagland_Expressions.txt";
+	
+	public static String runTestsOnExpression(String exp) {
+		ExpressionTree tree = new ExpressionTree(exp);
+		String str = tree.evalTree() + "\n" + tree.toPrefixNotation() + "\n" + tree.toInfixNotation() + "\n" + tree.toPostfixNotation() + "\n" + tree.postfixEval(exp.split("\\s+")) + "\n\n\n";
+		return str;
+	}
+	
+	public static void printToOutputFile(String output) {
+		PrintWriter file = null;
+		try {
+			file = new PrintWriter(new File(outputFileName));
+		} catch (FileNotFoundException ex) {
+			System.out.println(ex);
+			System.exit(1);
+		}
+		
+		if (file != null) {
+			file.print(output);
+			file.close();
+		}
+	}
 	
 	public static void main(String[] args) {
 		
@@ -30,13 +54,11 @@ public class ExpressionTreeTests {
 			}
 		}
 		
+		String output = "";
 		for (String exp : exps) {
-			ExpressionTree tree = new ExpressionTree(exp);
-			System.out.println(tree.evalTree());
-			System.out.println(tree.toPrefixNotation());
-			System.out.println(tree.toInfixNotation());
-			System.out.println(tree.toPostfixNotation());
-			System.out.println(tree.postfixEval(exp.split("\\s+")) + "\n\n");
+			output += runTestsOnExpression(exp);
 		}
+		printToOutputFile(output.trim());
+		System.out.println("All expressions processed");
 	}
 }
